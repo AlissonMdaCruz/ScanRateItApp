7c6734ad26adf2d33ae88d837777213a7c11936b
Alisson Cruz <alissonmcruz@gmail.com>
2023-09-02 16:47:35 -0300

    [Add] inicio navigation drawer

--- app/src/main/java/com/akhenaton/scanrateitapp/MainActivity.kt
+++ app/src/main/java/com/akhenaton/scanrateitapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.akhenaton.scanrateitapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.akhenaton.scanrateitapp.common.BaseActivity
 import com.akhenaton.scanrateitapp.databinding.ActivityMainBinding
 import com.akhenaton.scanrateitapp.features.login.ui.LoginActivity
@@ -11,6 +14,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
     ActivityMainBinding::inflate
 ) {
 
+    private lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,18 +23,34 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
         val currentUser = auth.currentUser
         if (currentUser == null) {
             goToLogin()
-        } else {
-            binding.txtUserDetails.text = currentUser.displayName
         }
 
-        binding.btnLogout.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-            goToLogin()
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> Toast.makeText(this, "item 1", Toast.LENGTH_SHORT).show()
+                R.id.nav_ratings -> Toast.makeText(this, "item 2", Toast.LENGTH_SHORT).show()
+                R.id.nav_my_data -> Toast.makeText(this, "item 3", Toast.LENGTH_SHORT).show()
+                R.id.nav_logout -> Toast.makeText(this, "item 4", Toast.LENGTH_SHORT).show()
+            }
+            true
         }
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun goToLogin() {
-        val intent = Intent(this@MainActivity, LoginActivity::class.java)
+        val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
         finish()
     }
