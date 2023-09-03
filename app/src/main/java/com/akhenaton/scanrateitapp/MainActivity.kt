8a2861b8ff0ee0bc837535bfd1bd57525f9543bf
Alisson Cruz <alissonmcruz@gmail.com>
2023-09-03 14:28:31 -0300

    [Fix] logout

--- app/src/main/java/com/akhenaton/scanrateitapp/MainActivity.kt
+++ app/src/main/java/com/akhenaton/scanrateitapp/MainActivity.kt
@@ -2,7 +2,7 @@ package com.akhenaton.scanrateitapp
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.drawerlayout.widget.DrawerLayout
+import android.widget.TextView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -11,37 +11,57 @@ import androidx.navigation.ui.setupWithNavController
 import com.akhenaton.scanrateitapp.common.BaseActivity
 import com.akhenaton.scanrateitapp.databinding.ActivityMainBinding
 import com.akhenaton.scanrateitapp.features.login.ui.LoginActivity
-import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : BaseActivity<ActivityMainBinding>(
     ActivityMainBinding::inflate
 ) {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private val navController by lazy {
+        findNavController(R.id.nav_host_fragment_content_main)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checksUserLoggedIn()
+        setupAppBarNavController()
+        setListener()
+    }
 
+    private fun checksUserLoggedIn() {
         val auth = FirebaseAuth.getInstance()
-        val currentUser = auth.currentUser
-        if (currentUser == null) {
-            goToLogin()
+        when (val currentUser = auth.currentUser) {
+            null -> goToLogin()
+            else -> showUserInfo(currentUser)
         }
+    }
 
+    private fun showUserInfo(currentUser: FirebaseUser) {
+        val headerView = binding.navView.getHeaderView(FIRST_INDEX)
+        val txtName = headerView.findViewById<TextView>(R.id.txt_header_name)
+        val txtEmail = headerView.findViewById<TextView>(R.id.txt_header_email)
+        txtName.text = currentUser.displayName
+        txtEmail.text = currentUser.email
+    }
+
+    private fun setupAppBarNavController() {
         setSupportActionBar(binding.appBarMain.toolbar)
-
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_ratings, R.id.nav_my_data, R.id.nav_logout
-            ), drawerLayout
+                R.id.nav_home, R.id.nav_ratings, R.id.nav_my_data
+            ), binding.drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        binding.navView.setupWithNavController(navController)
+    }
+
+    private fun setListener() {
+        binding.navView.menu.findItem(R.id.nav_logout).setOnMenuItemClickListener {
+            logOut()
+            true
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -58,5 +78,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    companion object {
+        private const val FIRST_INDEX = 0
     }
 }
