070c00725548b712ece2317e7ffd79253cfced8b
Alisson Cruz <alissonmcruz@gmail.com>
2023-09-15 09:33:14 -0300

    [Add] layout produto

--- app/src/main/java/com/akhenaton/scanrateitapp/features/product/ui/ProductFragment.kt
+++ app/src/main/java/com/akhenaton/scanrateitapp/features/product/ui/ProductFragment.kt
@@ -2,6 +2,8 @@ package com.akhenaton.scanrateitapp.features.product.ui
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.RatingBar
+import android.widget.Toast
 import com.akhenaton.scanrateitapp.common.BaseFragment
 import com.akhenaton.scanrateitapp.databinding.FragmentProductBinding
 import com.akhenaton.scanrateitapp.features.product.model.ProductModel
@@ -9,6 +11,13 @@ import com.akhenaton.scanrateitapp.features.product.model.ProductModel
 class ProductFragment : BaseFragment<FragmentProductBinding>() {
     override fun initView() {
         showDetails()
+        setlisteners()
+    }
+
+    private fun setlisteners() {
+        binding.rbrProduct.setOnClickListener {
+            Toast.makeText(requireContext(), "${(it as RatingBar).rating}", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun viewBinding(
@@ -18,7 +27,9 @@ class ProductFragment : BaseFragment<FragmentProductBinding>() {
 
     private fun showDetails() {
         val product = arguments?.getParcelable<ProductModel>(PRODUCT)
+        binding.txtProductCode.text = product?.code
         binding.txtProductName.text = product?.nome
+        binding.rbrProduct.rating = 4.5F
     }
 
     companion object {
